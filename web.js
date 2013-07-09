daa7dcf85634fa47ccd4bcf3aef3cbeb178142da
Tom Giordano <justom@gmail.com>
2013-07-09 01:59:28 -0400

    Read the text from index.html instead of hard-coding it in the js.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    buf = new Buffer(32);
+    buf.write(fs.readFileSync('index.html', 'ascii'));
+    response.send(buf.toString());
+  // response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
